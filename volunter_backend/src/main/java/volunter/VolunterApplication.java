6663d252e3a02689b6825bac85a9d0038808da47
Shiva kumar <shiva.kumar@theleanapps.com>
2024-01-21 22:22:22 +0530

    Initial commit

---
+++ volunter_backend/src/main/java/volunter/VolunterApplication.java
@@ -0,0 +1,13 @@
+package volunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunterApplication.class, args);
+	}
+
+}
---
+++ volunter_backend/src/test/java/volunter/VolunterApplicationTests.java
@@ -0,0 +1,13 @@
+package volunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
